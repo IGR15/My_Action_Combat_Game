d755fafb615e24e6d77d2680d4005072ba0b479e
omar switi <omarswiti10@gmail.com>
2025-07-02 23:48:28 +0300

    TP_Compontent

--- ActionCombat/Private/Characters/TeleportComponent.cpp
+++ ActionCombat/Private/Characters/TeleportComponent.cpp
@@ -2,26 +2,127 @@
 
 
 #include "Characters/TeleportComponent.h"
+#include "GameFramework/Actor.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "DrawDebugHelpers.h"
 
-// Sets default values for this component's properties
 UTeleportComponent::UTeleportComponent()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
 }
 
-
-// Called when the game starts
 void UTeleportComponent::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
-	
+	OwnerCharacter = Cast<ACharacter>(GetOwner());
+	if (TeleportAura)
+	{
+		TeleportAura->SetVisibility(false);
+	}
+
 }
+
+void UTeleportComponent::StartTeleportAim()
+{
+	bIsAiming = true;
+	if (TeleportAura)
+	{
+		TeleportAura->SetVisibility(true);
+	}
+}
+
+void UTeleportComponent::UpdateTeleportAim(float DeltaTime)
+{
+	if (!bIsAiming || !OwnerCharacter) return;
+	PerformAimTrace();
+}
+
+void UTeleportComponent::CancelTeleport()
+{
+	bIsAiming = false;
+	bCanTeleport = false;
+	if (TeleportAura)
+	{
+		TeleportAura->SetVisibility(false);
+	}
+}
+
+void UTeleportComponent::CompleteTeleport()
+{
+	if (!bIsAiming || !bCanTeleport || !OwnerCharacter) return;
+	bIsAiming = false;
+	TeleportAura->SetVisibility(false);
+	StartLocation = OwnerCharacter->GetActorLocation();
+}
+
+void UTeleportComponent::PerformAimTrace()
+{
+	FVector Start = TraceStart ? TraceStart->GetComponentLocation() : OwnerCharacter->GetActorLocation();
+	FVector End = Start + OwnerCharacter->GetActorForwardVector() * MaxTraceDistance;
+
+	FHitResult Hit;
+	FCollisionQueryParams Params;
+	Params.AddIgnoredActor(OwnerCharacter);
+
+	bool bHit = GetWorld()->LineTraceSingleByChannel(Hit, Start, End, ECC_Visibility, Params);
+	if (bHit)
+	{
+		PerformGroundCheck(Hit.ImpactPoint);
+	}
+	else
+	{
+		UpdateAura(FVector::ZeroVector, false);
+		bCanTeleport = false;
+	}
+}
+
+void UTeleportComponent::PerformGroundCheck(const FVector& AimPoint)
+{
+	FVector GroundStart = AimPoint + FVector(0, 0, 50);
+	FVector GroundEnd = AimPoint - FVector(0, 0, DownTraceDepth);
+
+	FHitResult GroundHit;
+	FCollisionQueryParams Params;
+	Params.AddIgnoredActor(OwnerCharacter);
+
+	bool bGround = GetWorld()->LineTraceSingleByChannel(GroundHit, GroundStart, GroundEnd, ECC_Visibility, Params);
+	if (bGround)
+	{
+		TeleportTargetLocation = GroundHit.ImpactPoint;
+		UpdateAura(TeleportTargetLocation, true);
+		bCanTeleport = true;
+	}
+	else
+	{
+		UpdateAura(FVector::ZeroVector, false);
+		bCanTeleport = false;
+	}
+}
+
+void UTeleportComponent::UpdateAura(const FVector& Location, bool bVisible)
+{
+	if (!TeleportAura) return;
+	TeleportAura->SetWorldLocation(Location);
+	TeleportAura->SetVisibility(bVisible);
+}
+
+void UTeleportComponent::ExecuteTeleport()
+{
+	if (OwnerCharacter)
+	{
+		OwnerCharacter->SetActorLocation(TeleportTargetLocation);
+	}
+}
+
+void UTeleportComponent::TimelineUpdate(float Value)
+{
+	if (!OwnerCharacter) return;
+	FVector NewLocation = FMath::Lerp(StartLocation, TeleportTargetLocation, Value);
+	OwnerCharacter->SetActorLocation(NewLocation);
+}
+
 
 
 // Called every frame
--- ActionCombat/Public/Characters/TeleportComponent.h
+++ ActionCombat/Public/Characters/TeleportComponent.h
@@ -1,28 +1,62 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
+// TeleportComponent.h
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "GameFramework/Character.h"
+#include "Curves/CurveFloat.h"
 #include "TeleportComponent.generated.h"
 
-
-UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class ACTIONCOMBAT_API UTeleportComponent : public UActorComponent
 {
 	GENERATED_BODY()
 
-public:	
-	// Sets default values for this component's properties
+public:
 	UTeleportComponent();
 
+	void StartTeleportAim();
+	void UpdateTeleportAim(float DeltaTime);
+	void CompleteTeleport();
+	void CancelTeleport();
+
 protected:
-	// Called when the game starts
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+private:
+	// Configurable properties
+	UPROPERTY(EditAnywhere)
+	USceneComponent* TeleportAura;
 
-		
+	UPROPERTY(EditAnywhere)
+	USceneComponent* TraceStart;
+
+	UPROPERTY(EditAnywhere)
+	float MaxTraceDistance = 1000.0f;
+
+	UPROPERTY(EditAnywhere)
+	float DownTraceDepth = 500.0f;
+
+	UPROPERTY(EditAnywhere)
+	UCurveFloat* TeleportCurve;
+
+	// Internal state
+	FVector TeleportTargetLocation;
+	FVector StartLocation;
+	bool bIsAiming = false;
+	bool bCanTeleport = false;
+	
+
+	// Cached Owner Character
+	ACharacter* OwnerCharacter;
+
+	// Helpers
+	void PerformAimTrace();
+	void PerformGroundCheck(const FVector& AimPoint);
+	void UpdateAura(const FVector& Location, bool bVisible);
+	void ExecuteTeleport();
+	void TimelineUpdate(float Value);
+
+public:
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 };
